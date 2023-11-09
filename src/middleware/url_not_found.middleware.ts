9203c453ac66123a049498ea23e1246c3c491d41
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-11-09 07:36:57 +0100

    Update route not found middleware

--- src/middleware/url_not_found.middleware.ts
+++ src/middleware/url_not_found.middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { STATUS_CODES, MESSAGES } from '../constants';
 
-// Handle 404 errors
+// Handle 404 route not found errors
 export default function handleRouteNotFound(req: Request, res: Response, next: NextFunction) {
   return res
     .status(404)
